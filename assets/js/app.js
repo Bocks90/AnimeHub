733096a0a9a94f83be08175413da4f4667435189
Bocks90 <bockariekoroma5@gmail.com>
2021-05-23 14:45:46 +0000

    added a click function to the button and the input value in the search bar is processing the value on the console log

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+// Selected elements from Dom 
+
+const buttonElement = document.querySelector('#search')
+const inputElement = document.querySelector('#inputValue')
+
+buttonElement.onclick = function(event) {
+    event.preventDefault();
+    const value = inputElement.value;
+    console.log('value:', value);
+}
+// add preventDefault to make sure the page doesnt refresh once button is clicked
+// input text appears on console log. This shows the value is being inputed into the console
+// console.log to test click 
